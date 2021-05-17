f179be0034a8fa68e8ac17bee1c3871847bef4f9
LeandroDreary <l3androviegas@gmail.com>
2021-05-17 20:38:59 -0300

    V2.6.7

--- database/connection.ts
+++ database/connection.ts
@@ -151,7 +151,11 @@ async function dbConnect() {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
-    maxPoolSize: 10
+    maxPoolSize: 10,
+    poolSize: 1, // Maintain up to 10 socket connections
+    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    keepAlive: false
   })
 
   connection.isConnected = db.connections[0].readyState
