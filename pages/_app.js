fd9d96d18f27c789dcc6f78d8409e9ec6bba65b6
LeandroDreary <l3androviegas@gmail.com>
2021-05-31 04:16:10 -0300

    V6.0.1

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import 'suneditor/dist/css/suneditor.min.css'
-import './../public/css/suneditorCustomClasses.css'
 import "tailwindcss/tailwind.css"
+import './../public/css/suneditorCustomClasses.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
