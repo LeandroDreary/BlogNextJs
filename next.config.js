22f345d8a78a81128abf8ae090d9ca8b003915e9
LeandroDreary <l3androviegas@gmail.com>
2021-06-04 17:24:08 -0300

    v6.9.5

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
   
       // Do not include .native which tries to load pg-native
       // See: https://github.com/sequelize/sequelize/issues/3781#issuecomment-537979334
-      config.plugins.push(new webpack.IgnorePlugin(/^pg-native$/))
+      config.plugins.push(new webpack.IgnorePlugin(/^mongodb-client-encryption$/))
   
       // Important: return the modified config
       return config
