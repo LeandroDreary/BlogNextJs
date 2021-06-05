1b72404dfb2fd2cdd5ec893f26ecbb9880967da5
LeandroDreary <l3androviegas@gmail.com>
2021-06-04 21:01:58 -0300

    v7.0.1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-// next.config.js
-
 module.exports = {
-    // Target must be serverless
     target: 'serverless',
     webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
       config.plugins.push(new webpack.IgnorePlugin(/^mongodb-client-encryption$/))
