9c38bff291c5198ae6b3938e2b2e39bb376159ae
LeandroDreary <l3androviegas@gmail.com>
2021-06-10 21:06:20 -0300

    V7.3.8.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,16 @@ module.exports = {
   purge: {
     content: ["./**/*.{ts,tsx}"],
     options: {
-      whitelist: [
-        /^bg-/,
-        /^text-/,
-        /^border-/,
-        /^from-/,
-        /^to-/,
-        /^placeholder-/
-      ]
+      whitelist: {
+        greedy: [
+        /$bg-$/,
+        /$text-$/,
+        /$border-$/,
+        /$from-$/,
+        /$to-$/,
+        /$placeholder-$/
+        ]
+      }
     }
   },
   darkMode: false, // or 'media' or 'class'
