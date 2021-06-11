19a53b32f13f7dc1b37d3a860f92f1d516386326
LeandroDreary <l3androviegas@gmail.com>
2021-06-10 22:19:24 -0300

    V7.3.15

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,16 @@ module.exports = {
   purge: {
     content: ["./**/*.{ts,tsx}"],
     options: {
-      safelist: [
-        /^bg-/,
-        /^hover:bg-/,
-        /^text-/,
-        /^hover:text-/,
-        /^border-/,
-        /^hover:border-/,
-        /^from-/,
-        /^hover:from-/,
-        /^to-/,
-        /^hover:to-/,
-        /^placeholder-/,
-        /^hover:placeholder-/,
-        "bg-black",
-        "bg-white",
-      ]
+      safelist: {
+        greedy: [
+          /$bg-$/,
+          /$text-$/,
+          /$border-$/,
+          /$from-$/,
+          /$to-$/,
+          /$placeholder-$/,
+        ]
+      }
     }
   }
   ,
